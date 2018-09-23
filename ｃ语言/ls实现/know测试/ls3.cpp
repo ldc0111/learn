af1cd980f755983dcf6d45de10ff6052372e3d9d
litesla <913361300@qq.com>
2018-09-23 08:42:35 +0800

    ls4-ls4_1

--- ｃ语言/ls实现/know测试/ls3.cpp
+++ ｃ语言/ls实现/know测试/ls3.cpp
@@ -16,11 +16,17 @@
 #include <stdlib.h>
 using namespace std;
 
+#define BROWN                "\e[0;33m"
+#define NONE                 "\e[0m"
+#define BLUE                 "\e[0;34m"
+
 void do_ls(char [], int);
 void print(char *ch);
-void dostat(char *,char *);
-
-
+void dostat(char *,char *,int);
+void show_stat_info(char *fname, struct stat *buf,int gon);
+void mode_to_letters(int mode, char str[]);
+char *uid_to_name(uid_t);
+char *gid_to_name(gid_t);
 
 int main(int ac,char *av[]){
     int temp = 0,i = 2; //00 低位是ａ，高位是ｌ
@@ -55,6 +61,7 @@ int comp(const void *a, const void *b){
 void do_ls(char *dirname, int gon){
     DIR *dir_ptr;
     struct dirent *direntp;
+    struct stat dir;
     char name[1000][255];
     int tn = 0;
     
@@ -76,12 +83,95 @@ void do_ls(char *dirname, int gon){
     }
     qsort(name,tn,sizeof(name[0]),comp);
     for(int i = 0; i < tn; i++){
-        if(gon & 2){
-            dostat(name[i],dirname);
-        }
-        else{
-            print(name[i]);
-        }
+        dostat(name[i],dirname,gon);
+    }
+    printf("\n");
+}
+void dostat( char *filename ,char dirname[],int gon){
+    struct stat info;
+    char pwdname[1000];
+    
+    strcpy(pwdname,dirname);
+    if(strcmp(dirname,".") == 0){
+        strcpy(pwdname,filename);
+    } else if (strcmp(dirname,"/") == 0){
+        strcat(pwdname,filename);
+    } else {
+        strcat(strcat(pwdname,"/"), filename);
+    }
+    
+
+    if(stat(pwdname, &info) == -1){
+        perror(filename);
+    } else {
+        show_stat_info(filename, &info,gon);
+    }
+}
+
+void show_stat_info(char *filename, struct stat *info_p,int gon){
+    char modestr[11];
+
+    mode_to_letters(info_p->st_mode, modestr);
+    if(gon & 2){
+        printf("%s", modestr);
+        printf("%4d ", (int)info_p->st_nlink);
+        printf("%-8s ", uid_to_name(info_p->st_uid));
+        printf("%-8s ", gid_to_name(info_p->st_gid));
+        printf("%8ld ", (long) info_p->st_size);
+        printf("%.12s ", 4 + ctime(&info_p->st_mtime));
+
+        if (modestr[0] == 'd') printf(BROWN "%s\n" NONE, filename);
+        else if(modestr[0] == '-') printf(BLUE "%s\n" NONE, filename);
+
+
+    }else{
+        if (modestr[0] == 'd') printf(BROWN "%s " NONE, filename);
+        else if(modestr[0] == '-') printf(BLUE "%s " NONE, filename);
+
+    }
+
+}
+void mode_to_letters(int mode, char str[]){
+    strcpy(str,"----------");
+
+    if (S_ISDIR(mode)) str[0] = 'd';
+    if (S_ISCHR(mode)) str[0] = 'c';
+    if (S_ISBLK(mode)) str[0] = 'b';
+
+    if (mode & S_IRUSR) str[1] = 'r';
+    if (mode & S_IWUSR) str[2] = 'w';
+    if (mode & S_IXUSR) str[3] = 'x';
+
+    if (mode & S_IRGRP) str[4] = 'r';
+    if (mode & S_IWGRP) str[5] = 'w';
+    if (mode & S_IXGRP) str[6] = 'x';
+
+    if (mode & S_IROTH) str[7] = 'r';
+    if (mode & S_IWOTH) str[8] = 'w';
+    if (mode & S_IXOTH) str[9] = 'x';
+
+}
+
+char* uid_to_name(uid_t uid){
+    struct passwd *pw_ptr;
+    static char numstr[10];
+
+    if ((pw_ptr = getpwuid(uid)) == NULL){
+        sprintf(numstr,"%d", uid);
+        return numstr;
+    }else{
+        return pw_ptr->pw_name;
+    }
+}
+char* gid_to_name(gid_t gid){
+    struct group *grp_ptr;
+    static char numstr[10];
+    
+    if((grp_ptr = getgrgid(gid)) == NULL){
+        sprintf(numstr,"%d", gid);
+        return numstr;
+    }else{
+        return grp_ptr->gr_name;
     }
 }
 
---
+++ ｃ语言/ls实现/know测试/ls4.cpp
@@ -0,0 +1,377 @@
+/*************************************************************************
+	> File Name: ls4.cpp
+	> Author: ldc
+	> Mail: litesla
+	> Created Time: 2018年09月22日 星期六 14时17分22秒
+ ************************************************************************/
+
+#include<iostream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <time.h>
+#include <pwd.h>
+#include <grp.h>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Macro definition
+
+#define NONE                 "\e[0m"
+#define BLACK                "\e[0;30m"
+#define L_BLACK              "\e[1;30m"
+#define RED                  "\e[0;31m"
+#define L_RED                "\e[1;31m"
+#define GREEN                "\e[0;32m"
+#define L_GREEN              "\e[1;32m"
+#define BROWN                "\e[0;33m"
+#define YELLOW               "\e[1;33m"
+#define BLUE                 "\e[0;34m"
+#define L_BLUE               "\e[1;34m"
+#define PURPLE               "\e[0;35m"
+#define L_PURPLE             "\e[1;35m"
+#define CYAN                 "\e[0;36m"
+#define L_CYAN               "\e[1;36m"
+#define GRAY                 "\e[0;37m"
+#define WHITE                "\e[1;37m"
+
+#define BOLD                 "\e[1m"
+#define UNDERLINE            "\e[4m"
+#define BLINK                "\e[5m"
+#define REVERSE              "\e[7m"
+#define HIDE                 "\e[8m"
+#define CLEAR                "\e[2J"
+#define CLRLINE              "\r\e[K" //or "\e[1K\r"
+
+
+#define maxn 1000
+#define chlen 255
+//end///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Structure declaration
+typedef struct Node{
+    char name[chlen];
+    char pwdname[chlen];
+    char jur[11];
+    int nlink;
+    long  size;
+    char ctim[chlen];
+    struct stat filestat;
+    char uidname[chlen];
+    char gidname[chlen];
+
+}Node;
+
+typedef struct forlen0{
+    int uid;
+    int gid;
+    int links;
+}forlen0;
+
+//end///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Function declaration
+
+//Function1
+//main
+//
+void init();
+
+
+//Function2
+//get_filename
+//得到每个文件的名字　完成后续操作
+void do_ls(char *, int);
+
+//Function3
+//dostat
+//得到每个文件的具体信息，和一些字符串的转化，统计对齐格式
+void dostat(char *, int);
+
+
+//Function4
+//mod_to_letters
+//将权限转化为字符串
+void mod_to_letters(int, char *);
+
+//Function41
+//show_stat_info
+//将信息输入进去
+void show_stat_info(int,struct stat*);
+//Function5
+//uid_to_name
+//将uid转化成对应的名字　获取对齐长度
+void uid_to_name(int,uid_t);
+
+//Function6
+//gid_to_name
+//将gid转化成对应的名字　获取对齐长度
+void gid_to_name(int,gid_t);
+
+//Function7
+//print1
+//普通输出
+void print1(int);
+
+//Function8
+//print2
+//长格式输出
+void print2(int);
+
+//end///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Global variable declaration
+Node files[maxn];
+forlen0 forlen;
+int op;//00 la
+int total;
+//end///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+int main(int ac,char *av[]){
+    init();
+    int opp = 0, i = 2; //00 la
+
+    *(++av);
+    for(i = 2; i <= ac && (*av)[0] == '-'; *(++av),i++){
+         strlen(*av) == 2 && (*av)[1] =='a'&& (opp |=1,1)||(*av)[1] == 'l' && (opp |= 2,1);
+         strlen(*av) == 3 && ((*av)[1] == 'a' && (*av)[2] == 'l' ||
+                              (*av)[1] == 'l' && (*av)[2] == 'a') && (opp |= 3,1);
+    }
+
+    op = opp;
+
+    if (i > ac) {                        //默认文件夹
+        do_ls(".", op);
+    } else if (i == ac) {                //指定一个文件夹
+        do_ls(*av, op);
+    }
+    else {
+        while(i <= ac){                 //指定多个文件夹
+            total = 0;
+            printf("%s:\n", *av);
+            do_ls(*av, op);
+            *(++av);
+            i++;
+            printf("\n");
+        }
+
+    }
+    return 0;
+}
+void init(){
+    memset(files,0,sizeof(Node)*maxn);
+    forlen.uid = 0;
+    forlen.gid = 0;
+    forlen.links = 0;
+}
+
+
+//Function2
+//get_filename
+//得到每个文件的名字 完成后续操作
+int comp(const void *a, const void *b){
+    return strcmp((*(Node*)a).name, (*(Node*)b).name);
+}
+
+void  do_ls(char *dirname, int op){
+    DIR *dir_ptr;
+    struct dirent *direntp;
+    struct stat dir;
+    int tn = 0;
+    
+
+    if ((dir_ptr = opendir(dirname)) == NULL) {
+        fprintf(stderr, "ls3:connot open %s\n", dirname);
+    } else {
+       
+        while((direntp = readdir(dir_ptr)) != NULL){
+            if(op & 1){//1
+                strcpy(files[tn].name,direntp->d_name);
+                tn++;
+            }else{
+                if(direntp->d_name[0] == '.') continue;
+                strcpy(files[tn].name,direntp->d_name);
+                tn++;
+            }
+        }
+        
+    }
+
+    qsort(files,tn,sizeof(files[0]),comp);
+    for(int i = 0; i < tn; i++) dostat(dirname, i);
+    
+    if(op & 2) print2(tn);
+    else print1(tn);
+    
+}
+
+//Function3
+//dostat
+//得到每个文件的具体信息，和一些字符串的转化，统计对齐格式
+void dostat(char *dirname,int i){
+    struct stat info;
+    char pwdname[1000];
+    
+    strcpy(pwdname,dirname);
+    if(strcmp(dirname,".") == 0){
+        strcpy(pwdname,files[i].name);
+    } else if (strcmp(dirname,"/") == 0){
+        strcat(pwdname,files[i].name);
+    } else {
+        strcat(strcat(pwdname,"/"), files[i].name);
+    }
+    strcpy(files[i].pwdname,pwdname);
+
+    if(stat(pwdname, &info) == -1){
+        perror(files[i].name);
+    } else {
+        files[i].nlink = (int)info.st_nlink;
+        files[i].size = (long)info.st_size;
+        strcpy(files[i].ctim, 4 + ctime(&info.st_mtime));
+        total += info.st_blocks/2;
+        forlen.links = forlen.links < log10((int)info.st_nlink) ? log10((int)info.st_nlink) : forlen.links;
+
+        show_stat_info(i,&info);
+    }    
+}
+
+//Function41
+//show_stat_info
+//将信息输入进去
+void show_stat_info(int i,struct stat* info_p){
+    mod_to_letters(info_p->st_mode, files[i].jur);
+    uid_to_name(i,info_p->st_uid);
+    gid_to_name(i,info_p->st_gid);
+}
+
+//Function4
+//mod_to_letters:w
+//将权限转化为字符串
+void mod_to_letters(int mode, char *str){
+    strcpy(str,"----------");
+
+    if (S_ISDIR(mode)) str[0] = 'd';
+    if (S_ISCHR(mode)) str[0] = 'c';
+    if (S_ISBLK(mode)) str[0] = 'b';
+
+    if (mode & S_IRUSR) str[1] = 'r';
+    if (mode & S_IWUSR) str[2] = 'w';
+    if (mode & S_IXUSR) str[3] = 'x';
+
+    if (mode & S_IRGRP) str[4] = 'r';
+    if (mode & S_IWGRP) str[5] = 'w';
+    if (mode & S_IXGRP) str[6] = 'x';
+
+    if (mode & S_IROTH) str[7] = 'r';
+    if (mode & S_IWOTH) str[8] = 'w';
+    if (mode & S_IXOTH) str[9] = 'x';    
+}
+
+//Function5
+//uid_to_name
+//将uid转化成对应的名字　获取对齐长度
+void uid_to_name(int i,uid_t uid){
+    struct passwd *pw_ptr;
+    static char numstr[10]; 
+
+    if((pw_ptr = getpwuid(uid)) == NULL){
+        sprintf(numstr,"%d", uid);
+
+        strcpy(files[i].uidname, numstr);
+        forlen.uid = forlen.uid < strlen(numstr) ? strlen(numstr):forlen.uid;
+    }else{
+        strcpy(files[i].uidname, pw_ptr->pw_name);
+        forlen.uid = forlen.uid < strlen(pw_ptr->pw_name) ? strlen(pw_ptr->pw_name):forlen.uid;
+    }
+    return ;
+}
+
+//Function6
+//gid_to_name
+//将gid转化成对应的名字　获取对齐长度
+void gid_to_name(int i, gid_t gid){
+    struct group *grp_ptr;
+    static char numstr[10]; 
+
+    if((grp_ptr = getgrgid(gid)) == NULL){
+        sprintf(numstr,"%d", gid);
+
+        strcpy(files[i].gidname, numstr);
+        forlen.gid = forlen.gid < strlen(numstr) ? strlen(numstr):forlen.gid;
+    }else{
+        strcpy(files[i].gidname, grp_ptr->gr_name);
+        forlen.gid = forlen.gid < strlen(grp_ptr->gr_name) ? strlen(grp_ptr->gr_name):forlen.gid;
+    }
+
+    return ;
+}
+
+//Function7
+//print1
+//普通输出
+void print1(int tn){
+    for(int i = 0; i< tn; i++){
+        if (files[i].jur[0] == 'd')
+            printf(YELLOW "%s  " NONE, files[i].name);
+        else if (files[i].jur[0] == 'c')
+            printf(RED "%s  " NONE, files[i].name);
+        else if (files[i].jur[0] == 'b')
+            printf(CYAN "%s  " NONE, files[i].name);
+        else if (files[i].jur[3] == 'x'||files[i].jur[6] == 'x'|| files[i].jur[9])
+            printf(L_BLUE "%s  " NONE,files[i].name);
+        else if (files[i].jur[0] == '-')
+            printf(BLUE "%s  " NONE, files[i].name);
+    }
+    printf("\n");
+}
+
+//Function8
+//print2
+//长格式输出
+void print2(int tn){
+    printf("total: %d\n",total);
+    for(int i = 0; i< tn; i++){
+        
+        printf("%s ", files[i].jur);
+        //printf("%d ",forlen.links);
+        printf("%-*d ", forlen.links+1,files[i].nlink);
+        //printf("%d",forlen.uid);
+        printf("%-*s ", forlen.uid+1, files[i].uidname);
+        printf("%-*s ", forlen.gid+1, files[i].gidname);
+        printf("%8ld ", files[i].size);
+        printf("%.12s ", files[i].ctim);
+        
+
+        if (files[i].jur[0] == 'd')
+            printf(YELLOW "%s\n" NONE, files[i].name);
+        else if (files[i].jur[0] == 'c')
+            printf(RED "%s\n" NONE, files[i].name);
+        else if (files[i].jur[0] == 'b')
+            printf(CYAN "%s\n" NONE, files[i].name);
+        else if (files[i].jur[3] == 'x'||files[i].jur[6] == 'x'|| files[i].jur[9])
+            printf(L_BLUE "%s\n" NONE,files[i].name);
+        else if (files[i].jur[0] == '-')
+            printf(BLUE "%s\n" NONE, files[i].name);
+    }
+}
+
+
+
+
